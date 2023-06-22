4da27431a0ead69d951244b4f08ed4cc12570f85
samuel-santinelli <samuelsantinelli@gmail.com>
2023-06-22 12:00:00 -0300

    feature: Added new label with location name

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -45,3 +45,6 @@ const Characters = ({ characters }) => {
 };
 
 export default Characters;
+<label className="bg-green-100 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
+  {item?.location?.name}
+</label>
