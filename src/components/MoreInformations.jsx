ac4a084e8ace023d1feb7b12476cd378853f72d4
samuel-santinelli <samuelsantinelli@gmail.com>
2023-06-25 12:00:00 -0300

    feature: Added new label with location name

--- src/components/MoreInformations.jsx
+++ src/components/MoreInformations.jsx
@@ -12,3 +12,6 @@ const MoreInformations = (data) => {
 };
 
 export default MoreInformations;
+<label className="bg-green-100 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
+  {item?.location?.name}
+</label>
