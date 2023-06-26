f44d93c95295ba7e236b1d7eaab76bdca1b8c5fa
samuel-santinelli <samuelsantinelli@gmail.com>
2023-06-26 12:00:00 -0300

    fix: Updated label style

--- src/components/MoreInformations.jsx
+++ src/components/MoreInformations.jsx
@@ -12,6 +12,6 @@ const MoreInformations = (data) => {
 };
 
 export default MoreInformations;
-<label className="bg-green-100 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
+<label className="bg-blue-200 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
   {item?.location?.name}
 </label>
