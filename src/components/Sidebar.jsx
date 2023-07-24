1c10f6cc59e4c63f1b6b98130bad76d8341d3734
samuel-santinelli <samuelsantinelli@gmail.com>
2023-07-24 12:00:00 -0300

    fix: Updated label style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -52,9 +52,9 @@ export default Sidebar;
 <label className="bg-blue-200 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
   {item?.location?.name}
 </label>
-<label className="bg-green-100 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
+<label className="bg-blue-200 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
   {item?.location?.name}
 </label>
-<label className="bg-green-100 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
+<label className="bg-blue-200 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
   {item?.location?.name}
 </label>
