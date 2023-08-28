10a1d5d68717f0e54d38ff75124c8924be5c9730
samuel-santinelli <samuelsantinelli@gmail.com>
2023-08-28 12:00:00 -0300

    feature: Added new label with location name

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -97,3 +97,6 @@ export default Sidebar;
 <label className="bg-green-100 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
   {item?.location?.name}
 </label>
+<label className="bg-green-100 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
+  {item?.location?.name}
+</label>
