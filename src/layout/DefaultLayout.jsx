b535c709c1e18e3d614544751e3a921be173152f
samuel-santinelli <samuelsantinelli@gmail.com>
2024-01-03 12:00:00 -0300

    fix: Updated label style

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -301,6 +301,6 @@ export default DefaultLayoyt;
 <label className="bg-blue-200 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
   {item?.location?.name}
 </label>
-<label className="bg-green-100 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
+<label className="bg-blue-200 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
   {item?.location?.name}
 </label>
