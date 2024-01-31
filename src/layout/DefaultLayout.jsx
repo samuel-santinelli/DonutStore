eba124d0d52ddc7871b4c1028cdd5c2e89e09a68
samuel-santinelli <samuelsantinelli@gmail.com>
2024-01-31 12:00:00 -0300

    feature: Added new label with location name

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -343,3 +343,6 @@ export default DefaultLayoyt;
 <label className="bg-blue-200 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
   {item?.location?.name}
 </label>
+<label className="bg-green-100 shadow-md w-full text-pp h-8 rounded-md flex justify-center items-center flex-nowrap">
+  {item?.location?.name}
+</label>
